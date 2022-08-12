3cafa0878b359235a284a52433ed4c397bfe6f4f
pavlo-shevchuk <pavlo.shevchuk@sombrainc.com>
2022-08-12 19:13:40 +0300

    add run migration

--- src/db/migration/1660307685548-add-default-admin.ts
+++ src/db/migration/1660307685548-add-default-admin.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class addDefaultAdmin1654530308279 implements MigrationInterface {
+export class foo1660307685548 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
         INSERT INTO admin(id, first_name, last_name, email, hash_password, is_active, role)
-        VALUES ('ad474c13-da54-489a-92d1-db11bbb79f72', 'Pavlo', 'Shevchuk', 'email@gmail.com',
+        VALUES ('bc506ec4-c6fa-40cc-93f4-cce62d4df80e', 'Pavlo', 'Shevchuk', 'shevchuk.pavlo1@gmail.com',
                 '$2a$10$AMO9v6Pjvi411Zwp8R3cBe84Fls4UHBGfAbk4H5n/5kvkCbZS7qLW', true, 'admin');
     `);
   }
@@ -13,7 +13,7 @@ export class addDefaultAdmin1654530308279 implements MigrationInterface {
     await queryRunner.query(`
         DELETE
         FROM admin
-        WHERE id = 'ad474c13-da54-489a-92d1-db11bbb79f72';
+        WHERE id = 'bc506ec4-c6fa-40cc-93f4-cce62d4df80e';
     `);
   }
 }
